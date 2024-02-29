7a13a605c6f9772194de74cccab19eb4f54eebe3
Carlos Mur <cmurdeveloper@gmail.com>
2024-02-29 16:19:22 +0100

    add: First commit

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/peliculasApp'),
+      dir: require('path').join(__dirname, './coverage/peliculas'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
